2279faf445c1979428bc8b54a57db1616d7a854c
AgalarovBaycan <106188929+AgalarovBaycan@users.noreply.github.com>
2022-06-28 21:02:43 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -3,7 +3,34 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
+    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
+    */
+    var input = result.input; // "5,2 4 7 12 9"
+    var arr = input.split(','); // ['5', '2 4 7 12 9']
+    const arr = [3,5,-7,7,5,-9,-4]
+let max = 0
+let min = arr[0]
+
+
+for(i = 0;i<arr.length;i++){
+    if(max<arr[i]){
+        max = arr[i]
+        index = i
+      
+    }
+    if(min>arr[i]){
+        min = arr[i]
+        indeks = i
+    }
+}
+
+let temp = arr[index] //7
+arr[index] = arr[indeks]
+arr[indeks] = temp;
+
+console.log(arr);
 });
+
