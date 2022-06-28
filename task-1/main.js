5935de30aec956d55ddf2b5a48b00c83a3c5b506
AgalarovBaycan <106188929+AgalarovBaycan@users.noreply.github.com>
2022-06-28 20:47:06 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -9,7 +9,7 @@ prompt.get('input', function (err, result) {
     Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
     */
     var input = result.input; // "5,2 4 7 12 9"
-    var arr = input.split(','); // ['5', '2 4 7 12 9']
+    var arr = input.split(''); // ['5', '2 4 7 12 9']
     let sum = 0
 let count = 0
 for (let i = 0; i < arr.length; i++) {
